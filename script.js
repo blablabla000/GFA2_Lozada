49101949da97e40159b41e1b6c7676510ca621fe
Anton Derek Lozada <125948423+blablabla000@users.noreply.github.com>
2023-04-11 20:00:37 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,61 +1,51 @@
-function add(){ // + option
-	//gets the inputs
+function add(){
 	var no1 = Number(document.getElementById("n1").value);
 	var no2 = Number(document.getElementById("n2").value);
 
-	//Calculates the recieved inputs
+
 	let X1 = no1 + no2;
 	let X2 = Number(X1.toFixed(2));
 
-	//Returns the output
 	document.getElementById("dum_result").style.display = "none";
 	document.getElementById("result").style.display = "block";
 	document.getElementById("presult").style.display = "block";
 	document.getElementById("presult").innerHTML = X2;
 }
 
-function sub(){ // - option
-	//gets the inputs
+function sub(){ 
 	var no1 = Number(document.getElementById("n1").value);
 	var no2 = Number(document.getElementById("n2").value);
 
-	//Calculates the recieved inputs
 	let X1 = no1 - no2;
 	let X2 = Number(X1.toFixed(2));
 
-	//Returns the output
 	document.getElementById("dum_result").style.display = "none";
 	document.getElementById("result").style.display = "block";
 	document.getElementById("presult").style.display = "block";
 	document.getElementById("presult").innerHTML = X2;
 }
 
-function mult(){ // x option
-	//gets the inputs
+function mult(){ 
 	var no1 = Number(document.getElementById("n1").value);
 	var no2 = Number(document.getElementById("n2").value);
 
-	//Calculates the recieved inputs
 	let X1 = no1 * no2;
 	let X2 = Number(X1.toFixed(2));
 
-	//Returns the output
+
 	document.getElementById("dum_result").style.display = "none";
 	document.getElementById("result").style.display = "block";
 	document.getElementById("presult").style.display = "block";
 	document.getElementById("presult").innerHTML = X2;
 }
 
-function div(){ // / option
-	//gets the inputs
+function div(){ 
 	var no1 = Number(document.getElementById("n1").value);
 	var no2 = Number(document.getElementById("n2").value);
 
-	//Calculates the recieved inputs
 	let X1 = no1/no2;
 	let X2 = Number(X1.toFixed(2));
 
-	//Returns the output
 	document.getElementById("dum_result").style.display = "none";
 	document.getElementById("result").style.display = "block";
 	document.getElementById("presult").style.display = "block";
@@ -63,15 +53,12 @@ function div(){ // / option
 }
 
 function mod(){
-	//gets the inputs
 	var no1 = Number(document.getElementById("n1").value);
 	var no2 = Number(document.getElementById("n2").value);
 
-	//Calculates the recieved inputs
 	let X1 = no1 % no2;
 	let X2 = Number(X1.toFixed(2));
 
-	//Returns the output
 	document.getElementById("dum_result").style.display = "none";
 	document.getElementById("result").style.display = "block";
 	document.getElementById("presult").style.display = "block";
